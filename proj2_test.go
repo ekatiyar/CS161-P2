d3b28a668e4b03bb53aa042372e9fd19ee0e45c2
Wesley Hsiao <nova9000@berkeley.edu>
2020-03-28 23:34:10 +0800

    7 more flags needed

--- proj2_test.go
+++ proj2_test.go
@@ -13,7 +13,6 @@ import (
 	"testing"
 
 	"github.com/cs161-staff/userlib"
-	"github.com/google/uuid"
 	_ "github.com/google/uuid"
 )
 
@@ -21,6 +20,14 @@ func clear() {
 	// Wipes the storage so one test does not affect another
 	userlib.DatastoreClear()
 	userlib.KeystoreClear()
+}
+
+func mix() {
+	dsmap := userlib.DatastoreGetMap()
+	randomitem := userlib.RandomBytes(16)
+	for i, _ := range dsmap {
+		userlib.DatastoreSet(i, randomitem)
+	}
 }
 
 func TestInit(t *testing.T) {
@@ -189,19 +196,7 @@ func TestAdvGet(t *testing.T) {
 		return
 	}
 
-	m := userlib.DatastoreGetMap()
-	var uuids []uuid.UUID
-	for k := range m {
-		uuids = append(uuids, k)
-	}
-
-	one, _ := userlib.DatastoreGet(uuids[0])
-	two, _ := userlib.DatastoreGet(uuids[1])
-	if reflect.DeepEqual(one, two) {
-		t.Error("Stored entries are already equal???")
-	}
-	userlib.DatastoreSet(uuids[1], one)
-	userlib.DatastoreSet(uuids[0], two)
+	mix()
 
 	u2, err := GetUser("alice", "fubar")
 	if err == nil {
@@ -210,6 +205,153 @@ func TestAdvGet(t *testing.T) {
 	}
 }
 
+//play with datastore files when revoking, appending
+func TestDataStore(t *testing.T) {
+	clear()
+	mix()
+	t.Log("mixing datastore before init")
+	u1, err := InitUser("alice", "fubar")
+	if err != nil {
+		// t.Error says the test fails
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	u2, err := GetUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to get user", err)
+		return
+	}
+	equiv := reflect.DeepEqual(u1, u2)
+
+	if !equiv {
+		t.Error("Failed to get correct user", err)
+	}
+
+	t.Log("Get works!", u2)
+}
+
+func TestMixAfterLoad(t *testing.T) {
+	clear()
+	t.Log("Storage test mix")
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	v := []byte("This is a test")
+	u.StoreFile("file1", v)
+	mix()
+	v2, err2 := u.LoadFile("file1")
+	if err2 == nil {
+		t.Error("Failed to catch datastore corruption", err2)
+		return
+	}
+	if reflect.DeepEqual(v, v2) {
+		t.Error("Downloaded file is the same", v, v2)
+		return
+	}
+}
+
+func TestMixReceive(t *testing.T) {
+	clear()
+	t.Log("mix and share")
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	u2, err2 := InitUser("bob", "foobar")
+	if err2 != nil {
+		t.Error("Failed to initialize bob", err2)
+		return
+	}
+	v := []byte("This is a test")
+	u.StoreFile("file1", v)
+	var magic_string string
+
+	magic_string, err = u.ShareFile("file1", "bob")
+	if err != nil {
+		t.Error("Failed to share the a file", err)
+		return
+	}
+	mix()
+	err = u2.ReceiveFile("file2", "alice", magic_string)
+	if err == nil {
+		t.Error("Failed to catch share error", err)
+		return
+	}
+}
+
+func TestMixShare(t *testing.T) {
+	clear()
+	t.Log("mix and share")
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	u2, err2 := InitUser("bob", "foobar")
+	if err2 != nil {
+		t.Error("Failed to initialize bob", err2)
+		return
+	}
+	v := []byte("This is a test")
+	u.StoreFile("file1", v)
+	var magic_string string
+	mix()
+	magic_string, err = u.ShareFile("file1", "bob")
+	if err == nil {
+		t.Error("Failed to catch share corruption", err)
+		return
+	}
+	err = u2.ReceiveFile("file2", "alice", magic_string)
+	if err == nil {
+		t.Error("Failed to catch share error", err)
+		return
+	}
+}
+
+//test mix before revoke to prevent revoke
+func TestMixRevoke(t *testing.T) {
+	clear()
+	t.Log("mix og user to prevent revoking")
+
+	alice, err := InitUser("alice", "fubar")
+	if err != nil {
+		// t.Error says the test fails
+		t.Error("Failed to initialize user", err)
+		return
+	}
+
+	malice, err := InitUser("malice", "fubar")
+	if err != nil {
+		// t.Error says the test fails
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	v := []byte("This is a test")
+	alice.StoreFile("file1", v)
+
+	var magic_string string
+
+	magic_string, err = alice.ShareFile("file1", "malice")
+	if err != nil {
+		t.Error("Failed to share the a file", err)
+		return
+	}
+	err = malice.ReceiveFile("file2", "alice", magic_string)
+	if err != nil {
+		t.Error("Failed to receive the share message", err)
+		return
+	}
+	mix()
+	err = alice.RevokeFile("file1", "malice")
+	if err == nil {
+		t.Error("failed to catch corruption", err)
+		return
+	}
+
+}
 func TestStorage(t *testing.T) {
 	clear()
 	t.Log("Storage test")
@@ -253,6 +395,34 @@ func TestAppend(t *testing.T) {
 	}
 	if !reflect.DeepEqual(final, v2) {
 		t.Error("Downloaded file is not the same", v, v2)
+		return
+	}
+}
+
+func TestBigAppend(t *testing.T) {
+	clear()
+	t.Log("big append")
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+
+	v := []byte("This is a test")
+	u.StoreFile("file1", v)
+	vmore := userlib.RandomBytes(1500)
+	err = u.AppendFile("file1", vmore)
+	if err != nil {
+		t.Error("Failed to append", err)
+		return
+	}
+	v2, err2 := u.LoadFile("file1")
+	if err2 != nil {
+		t.Error("Failed to upload and download", err2)
+		return
+	}
+	if reflect.DeepEqual(v, v2) {
+		t.Error("no change", v, v2)
 		return
 	}
 }
@@ -359,6 +529,76 @@ func TestShare(t *testing.T) {
 	if !reflect.DeepEqual(v, v2) {
 		t.Error("Shared file is not the same", v, v2)
 		return
+	}
+}
+
+//test share cycle
+func TestShareCycle(t *testing.T) {
+	clear()
+	t.Log("Share test")
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	u2, err2 := InitUser("bob", "foobar")
+	if err2 != nil {
+		t.Error("Failed to initialize bob", err2)
+		return
+	}
+	u3, err3 := InitUser("cat", "c")
+	if err3 != nil {
+		t.Error("Failed to initialize bob", err3)
+		return
+	}
+	v := []byte("This is a test")
+	u.StoreFile("file1", v)
+
+	var magic_string string
+
+	magic_string, err = u.ShareFile("file1", "bob")
+	if err != nil {
+		t.Error("Failed to share the file to bob", err)
+		return
+	}
+	err = u2.ReceiveFile("file2", "alice", magic_string)
+	if err != nil {
+		t.Error("Failed to receive the share message from alice", err)
+		return
+	}
+	magic_string, err = u2.ShareFile("file2", "cat")
+	if err != nil {
+		t.Error("Failed to share the file to cat", err)
+		return
+	}
+	err = u3.ReceiveFile("file3", "bob", magic_string)
+	if err != nil {
+		t.Error("Failed to receive the share message from bob", err)
+		return
+	}
+	magic_string, err = u3.ShareFile("file3", "alice")
+	if err != nil {
+		t.Error("Failed to share the file to alice", err)
+		return
+	}
+	err = u.ReceiveFile("file4", "cat", magic_string)
+	if err != nil {
+		t.Error("Failed to receive the file form cat", err)
+		return
+	}
+	err = u.ReceiveFile("file5", "cat", magic_string)
+	if err != nil {
+		t.Error("Failed to receive the file form cat twice", err)
+		return
+	}
+	err = u.ReceiveFile("file1", "cat", magic_string)
+	if err != nil {
+		t.Error("failed to receive with same name")
+		return
+	}
+	err = u.RevokeFile("file1", "bob")
+	if err == nil {
+		t.Error("File Status updated not og author anymore", err)
 	}
 }
 
@@ -721,6 +961,62 @@ func TestRevoke(t *testing.T) {
 	_, err = u2.LoadFile("file2")
 	if err == nil {
 		t.Error("Still was able to download the file after revoke")
+		return
+	}
+}
+func TestOneUserTwoFileRevoke(t *testing.T) {
+	clear()
+	t.Log("Share test")
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	u2, err2 := InitUser("bob", "foobar")
+	if err2 != nil {
+		t.Error("Failed to initialize bob", err2)
+		return
+	}
+
+	v := []byte("This is a test")
+	v1 := []byte("second file")
+	u.StoreFile("file1", v)
+	u.StoreFile("file3", v1)
+
+	var magic_string string
+	var magic_string2 string
+	magic_string, err = u.ShareFile("file1", "bob")
+	if err != nil {
+		t.Error("Failed to share the a file", err)
+		return
+	}
+	err = u2.ReceiveFile("file2", "alice", magic_string)
+	if err != nil {
+		t.Error("Failed to receive the share message", err)
+		return
+	}
+	magic_string2, err = u.ShareFile("file3", "bob")
+	if err != nil {
+		t.Error("Failed to share the a file", err)
+		return
+	}
+	err = u2.ReceiveFile("file4", "alice", magic_string2)
+	if err != nil {
+		t.Error("Failed to receive the share message", err)
+		return
+	}
+	err = u.RevokeFile("file1", "bob")
+	if err != nil {
+		t.Error("Failed to revoke file", err)
+	}
+	_, err = u2.LoadFile("file4")
+	if err != nil {
+		t.Error("file4 should not be effected")
+		return
+	}
+	err = u2.AppendFile("file4", v)
+	if err != nil {
+		t.Error("file4 should be appended")
 		return
 	}
 }
